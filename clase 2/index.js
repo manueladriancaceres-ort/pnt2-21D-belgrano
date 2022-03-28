477c9750d7028c37dbde476ada20deb31af48617
manueladriancaceres-ort <manuel.caceres@ort.edu.ar>
2022-03-28 19:39:35 -0300

    ejercicio clase 2

--- clase 2/index.js
+++ clase 2/index.js
@@ -83,21 +83,23 @@ console.log(persona1.getListaTel());
 const proceso = {
     facturas: [{codigo:1,desc:"medias",importe: 1000},
                {codigo:2,desc:"zapas",importe: 20000},
-               {codigo:3,desc:"remera",importe: 5000}] ,
+               {codigo:3,desc:"remera",importe: 5000} ,
+               {codigo:4,desc:"pantalon",importe: 10000},
+               {codigo:5,desc:"campera",importe: 25000}] ,
     recibos: [ { codigo:1, numFacutra: 1, importe: 800},
                { codigo:2, numFacutra: 3, importe: 5000}, 
+               { codigo:3, numFacutra: 5, importe: 25000}, 
                   ],
-    getCalcularSaldoFacturas: function() {
-        // todas
+    getCalcularSaldoTodasLasFacturas: function() {
         const rta = []
         for(let i=0; i<this.facturas.length;i++) {
             const objSaldo = this.facturas[i];
-            objSaldo.saldo = 0;
+            objSaldo.saldo = objSaldo.importe;
             for(let j=0; j < this.recibos.length; j++) {
                 const recibo = this.recibos[j];
-                // falta comparar codigo de factura
-                // numFactura de recibo
-                objSaldo.saldo = objSaldo.importe - recibo.importe;
+                if(objSaldo.codigo===recibo.numFacutra) {
+                    objSaldo.saldo = objSaldo.importe - recibo.importe;
+                }    
             }
             rta.push(objSaldo);
         }
@@ -108,7 +110,7 @@ const proceso = {
     }
 }
 
-console.log(proceso.getCalcularSaldoFacturas());
+console.log(proceso.getCalcularSaldoTodasLasFacturas());
 
 
 
